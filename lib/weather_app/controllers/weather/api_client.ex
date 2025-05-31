b32a661f4d34965884c46bca3bf4084e2ef35c36
Mauricio <mjhenriquesb29@gmail.com>
2025-05-31 16:12:01 +0200

    Revert "add api_client for fetching cities information and weather information"
    This reverts commit 55b41a05ac858492e9352132013ae345412322ff.

--- lib/weather_app/controllers/weather/api_client.ex
+++ lib/weather_app/controllers/weather/api_client.ex
@@ -1,13 +1,8 @@
 defmodule WeatherApp.Controllers.Weather.ApiClient do
-  @moduledoc """
-  A client for fetching weather data from the OpenWeatherMap API.
-  """
 
-  @base_url Dotenv.get("OPENWEATHER_API_BASE_URL")
   @geo_url Dotenv.get("OPENWEATHER_API_GEO_URL")
   @api_key Dotenv.get("OPENWEATHER_API_KEY")
 
-  @spec search_cities(String.t()) :: {:ok, list(map())} | {:error, String.t()}
   def search_cities(query) do
     url = "#{@geo_url}/direct?q=#{query}&limit=10&appid=#{@api_key}"
 
@@ -16,25 +11,6 @@ defmodule WeatherApp.Controllers.Weather.ApiClient do
         case Jason.decode(response.body) do
           {:ok, decoded_response} ->
             validate_cities_response(decoded_response)
-          {:error, reason} ->
-            {:error, "JSON decode error: #{reason}"}
-        end
-      {:ok, %HTTPoison.Response{status_code: code}} ->
-        {:error, "Error: #{code}"}
-      {:error, %HTTPoison.Error{reason: reason}} ->
-        {:error, reason}
-    end
-  end
-
-  @spec get_weather_data(float(), float()) :: {:ok, map()} | {:error, String.t()}
-  def get_weather_data(lat, lon) do
-    url = "#{@base_url}/weather?lat=#{lat}&lon=#{lon}&appid=#{@api_key}"
-
-    case HTTPoison.get(url) do
-      {:ok, response} when response.status_code == 200 ->
-        case Jason.decode(response.body) do
-          {:ok, decoded_response} ->
-            {:ok, decoded_response}
           {:error, reason} ->
             {:error, "JSON decode error: #{reason}"}
         end

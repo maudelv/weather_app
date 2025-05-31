2d7f473b37d97852c5b43f8743114221f4ebafc0
Mauricio <mjhenriquesb29@gmail.com>
2025-05-31 23:30:54 +0200

    added interface for liveview

---
+++ lib/weather_app/controllers/weather/find_cities.ex
@@ -0,0 +1,23 @@
+defmodule WeatherApp.Controllers.Weather.FindCities do
+  @moduledoc """
+  Controller for finding cities based on a search query.
+  """
+
+  alias WeatherApp.Controllers.Weather.ApiClient
+
+  @doc """
+  Searches for cities based on the provided query.
+  """
+  def find_cities(query) when is_binary(query) and byte_size(query) >= 2 do
+    case ApiClient.search_cities(query) do
+      {:ok, cities} -> {:ok, cities}
+      {:error, reason} -> {:error, reason}
+    end
+  end
+
+  def format_city_options(cities) do
+    Enum.map(cities, fn city ->
+      "#{city["name"]}, #{city["country"]} (#{city["lat"]}, #{city["lon"]})"
+    end)
+  end
+end

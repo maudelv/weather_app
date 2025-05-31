a33f85b449d1e200737e35fa9b5043271ec748dd
Mauricio <mjhenriquesb29@gmail.com>
2025-06-01 01:45:27 +0200

    added module for accessing api client and fetching weather data

---
+++ lib/weather_app/controllers/weather_data.ex
@@ -0,0 +1,23 @@
+defmodule WeatherApp.Controllers.Weather.WeatherData do
+  @moduledoc """
+  Module for retrieving and formatting weather data.
+  """
+
+  alias WeatherApp.Controllers.Weather.ApiClient
+
+  def get_current_weather(lat, lon) do
+    case ApiClient.get_weather_data(lat, lon) do
+      {:ok, weather_data} -> format_weather_display(weather_data)
+      {:error, reason} -> {:error, reason}
+    end
+  end
+
+  defp format_weather_display(weather_data) do
+    %{
+      temperature: weather_data["main"]["temp"],
+      humidity: weather_data["main"]["humidity"],
+      description: weather_data["weather"] |> List.first() |> Map.get("description"),
+      wind_speed: weather_data["wind"]["speed"]
+    }
+  end
+end

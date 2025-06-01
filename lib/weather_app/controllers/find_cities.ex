e451a6ed8d05eba63bb220561d68e71bddb0540a
Mauricio <mjhenriquesb29@gmail.com>
2025-06-01 20:32:02 +0200

    enhance find_cities function to handle short and invalid queries

--- lib/weather_app/controllers/find_cities.ex
+++ lib/weather_app/controllers/find_cities.ex
@@ -15,6 +15,16 @@ defmodule WeatherApp.Controllers.Weather.FindCities do
     end
   end
 
+  # Handle queries shorter than 2 characters
+  def find_cities(query) when is_binary(query) and byte_size(query) < 2 do
+    {:ok, []}
+  end
+
+  # Catch-all for any other type of query, though less likely with LiveView input
+  def find_cities(_) do
+    {:ok, []}
+  end
+
   def format_city_options(cities) do
     Enum.map(cities, fn city ->
       "#{city["name"]}, #{city["country"]} (#{city["lat"]}, #{city["lon"]})"

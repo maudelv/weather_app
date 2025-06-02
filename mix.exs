23274dfb2996199a88eaabae1eb855cccb2c1f63
Mauricio <mjhenriquesb29@gmail.com>
2025-06-02 11:30:27 +0200

    change version, it's 1.17.3, not 1.14

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule WeatherApp.MixProject do
     [
       app: :weather_app,
       version: "0.1.0",
-      elixir: "~> 1.14",
+      elixir: "~> 1.17.3",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),

68db6512770f5a371b53c0ad908a850631d60354
Mauricio <mjhenriquesb29@gmail.com>
2025-06-02 11:29:51 +0200

    fix migrations

---
+++ priv/repo/migrations/20250601191846_drop_old_and_add_new_favorite_cities_index.exs
@@ -0,0 +1,9 @@
+defmodule WeatherApp.Repo.Migrations.DropOldAndAddNewFavoriteCitiesIndex do
+  use Ecto.Migration
+
+  def change do
+    drop index(:favorite_cities, [], name: :favorite_cities_country_code_state_lat_lon_index)
+
+    create unique_index(:favorite_cities, [:country_code, :name], name: :favorite_cities_country_code_name_index)
+  end
+end

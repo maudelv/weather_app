26e51c5b4840d41dc334d5988d270294fb2e5d67
Mauricio <mjhenriquesb29@gmail.com>
2025-06-03 10:36:17 +0200

    push version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule WeatherApp.MixProject do
   def project do
     [
       app: :weather_app,
-      version: "1.0.0",
+      version: "1.1.0",
       elixir: "~> 1.17.3",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
